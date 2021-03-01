dad12a27ee9a4c74a7bce229ff2d7261c5d2f65e
ValeriaMahdenko <vumahdenko6@gmail.com>
2021-03-01 16:42:42 +0200

    Create Currency Converter

---
+++ CurrencyConverter/CurrencyConverter.py
@@ -0,0 +1,18 @@
+import requests
+from secret_info import url
+
+
+class CurrencyConverter:
+    """ Based on https://fixer.io/ """
+    def __init__(self):
+        self.currencies = requests.get(url).json()
+        self.rates = self.currencies['rates']
+
+    def convert(self, from_currency, to_currency, amount):
+        try:
+            if from_currency != 'EUR':
+                amount = amount / self.rates[from_currency]
+            converted = amount * self.rates[to_currency]
+            return converted
+        except KeyError:
+            raise KeyError("Currency incorrect")
